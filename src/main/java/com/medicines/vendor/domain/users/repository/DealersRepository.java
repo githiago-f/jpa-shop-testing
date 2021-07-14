d38936edc35fa9ac147c00b91cda87e078612069
githiago-f <tfarias@protonmail.com>
2021-07-14 13:27:02 -0300

    ref: remove dealer object - will be created by provider

--- src/main/java/com/medicines/vendor/domain/users/repository/DealersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.medicines.vendor.domain.users.repository;
-
-import com.medicines.vendor.domain.users.Dealer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DealersRepository extends JpaRepository<Dealer, Long> {
-}
--- src/main/java/com/medicines/vendor/shared/errors/HandleUniqueFieldViolation.java
+++
@@ -1,7 +0,0 @@
-package com.medicines.vendor.shared.errors;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class HandleUniqueFieldViolation {
-}
